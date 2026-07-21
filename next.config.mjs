a9139ac04ce24e69a0644d32741fd9b657246b65
FitSIM <246489894+FitSIM@users.noreply.github.com>
2026-07-21 17:42:48 +0800

    fix: inline NEXT_PUBLIC erxes env vars into client bundle

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,12 @@ const nextConfig = {
     unoptimized: true,
   },
   env: {
+    NEXT_PUBLIC_GRAPHQL_URL: "https://landlockedmn.next.erxes.io/gateway/graphql",
     NEXT_PUBLIC_ERXES_ENDPOINT: "https://landlockedmn.next.erxes.io/gateway/graphql",
+    NEXT_PUBLIC_ERXES_API_URL: "https://landlockedmn.next.erxes.io",
+    NEXT_PUBLIC_ERXES_WSS_URL: "wss://landlockedmn.next.erxes.io",
     NEXT_PUBLIC_ERXES_CMS_ID: "6a3e1052efbb68d03bf95af5",
+    NEXT_PUBLIC_ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6ImtwTEdIbXBrSUR3LWJpMk5aNUNpWSIsImlhdCI6MTc4MjQ0ODY3OX0.KSvLtiYyyNDvU4zJG4j0ay2nowNnxtNo4tPQvcJlLOE",
     ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6ImtwTEdIbXBrSUR3LWJpMk5aNUNpWSIsImlhdCI6MTc4MjQ0ODY3OX0.KSvLtiYyyNDvU4zJG4j0ay2nowNnxtNo4tPQvcJlLOE",
   },
 };
